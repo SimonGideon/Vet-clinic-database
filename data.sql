f5e251d4e5e347dde235cadd954537cd8f14d831
Simon Gideon <simongideon918@gmail.com>
2023-01-24 02:02:39 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Agumon', DATE '2020-02-03',0, false, 10.23);
+VALUES('Gabumon', DATE '2018-11-15',2,true,8);
+VALUES('Pikachu', DATE '2021-01-07',1,false,15.04);
+VALUES('Devimon', DATE '2017-05-12',5,true,11);
