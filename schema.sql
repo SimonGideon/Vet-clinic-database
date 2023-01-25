3fc9ab14f49b404786a077a2779c4b8a39495416
Simon Gideon <simongideon918@gmail.com>
2023-01-25 16:44:02 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE  animals(
-id INT,
+CREATE TABLE  animal(
+id SERIAL PRIMARY KEY,
 name VARCHAR(50),
 date_of_birth DATE,
 escape_attempts INT,
