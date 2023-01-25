c95d0302898a20c2ebc6b33942658599ffbc0da4
Simon Gideon <simongideon918@gmail.com>
2023-01-25 21:29:15 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -6,3 +6,8 @@ VALUES('Agumon', DATE '2020-02-03',0, false, 10.23);
 VALUES('Gabumon', DATE '2018-11-15',2,true,8);
 VALUES('Pikachu', DATE '2021-01-07',1,false,15.04);
 VALUES('Devimon', DATE '2017-05-12',5,true,11);
+
+INSERT INTO animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Charmander', DATE '2020-02-08',0,false,11),('Plantmon',DATE '2021-11-15',2, TRUE, 5.7),('Squirtle', DATE '1993-02-04',3,FALSE,12.13),('Angemon',DATE '2003-06-12',1,TRUE,45),('Boarmon',DATE '2005-06-07',7,TRUE,20.4),('Blossom',DATE '1998-10-13',3,TRUE,17),('Ditto', DATE '2022-05-14',4,TRUE,22);
+
