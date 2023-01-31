fac6aef012ae87b34ed6f64d445d296f64e15b71
Simon Gideon <simongideon918@gmail.com>
2023-02-01 01:47:36 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -11,3 +11,6 @@ INSERT INTO animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Charmander', DATE '2020-02-08',0,false,11),('Plantmon',DATE '2021-11-15',2, TRUE, 5.7),('Squirtle', DATE '1993-02-04',3,FALSE,12.13),('Angemon',DATE '2003-06-12',1,TRUE,45),('Boarmon',DATE '2005-06-07',7,TRUE,20.4),('Blossom',DATE '1998-10-13',3,TRUE,17),('Ditto', DATE '2022-05-14',4,TRUE,22);
 
+-- insert values into owners
+INSERT INTO owners (full_name,age)
+VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19),('Bob', 45),('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
